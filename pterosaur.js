0b2ee7fd40e5227d29a52d9ddb33dcb3a8c45370
Ardagnir <jck1089@gmail.com>
2014-06-08 16:01:03 -0400

    Added empty shadowvim options argument.

--- pterosaur.js
+++ pterosaur.js
@@ -490,9 +490,9 @@ function startShadowvim(debug) {
   vimProcess.init(FileUtils.File('/bin/sh'));
   //Note: +clientserver doesn't work for some values of TERM (like linux)
   if (debug)
-    vimProcess.runAsync([ '-c',"TERM=xterm vim --servername pterosaur_"+uid+" +'call Shadowvim_SetupShadowvim(\"\")' </tmp/shadowvim/pterosaur_"+uid+"/fifo"],2);
+    vimProcess.runAsync([ '-c',"TERM=xterm vim --servername pterosaur_"+uid+" +'call Shadowvim_SetupShadowvim(\"\",\"\")' </tmp/shadowvim/pterosaur_"+uid+"/fifo"],2);
   else
-    vimProcess.runAsync([ '-c',"TERM=xterm vim --servername pterosaur_"+uid+" +'call Shadowvim_SetupShadowvim(\"\")' </tmp/shadowvim/pterosaur_"+uid+"/fifo >/dev/null"],2);
+    vimProcess.runAsync([ '-c',"TERM=xterm vim --servername pterosaur_"+uid+" +'call Shadowvim_SetupShadowvim(\"\",\"\")' </tmp/shadowvim/pterosaur_"+uid+"/fifo >/dev/null"],2);
 
   //We have to send SOMETHING to the fifo or vim will stay open when we close.
   io.system("echo -n ' ' > /tmp/shadowvim/pterosaur_"+uid+"/fifo")
